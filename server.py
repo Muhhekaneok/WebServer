a5bf525f8f8aa48d59a3aabf1383285f3ac9f44e
darkwizard <wizardrad@gmail.com>
2025-04-30 09:40:53 +0500

    modified

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-import http.server
-import socketserver
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "templates/index.html"
+        elif self.path.startswith("/static/"):
+            self.path = self.path.lstrip("/")
+        elif self.path.endswith(".js") or self.path.endswith(".css") or self.path.endswith(".png"):
+            self.path = self.path.lstrip("/")
+        else:
+            self.send_error(404, "File not found")
+            return
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        return super().do_GET()
+
+
+if __name__ == "__main__":
+    PORT = 8000
+    server_address = ("", PORT)
+    httpd = HTTPServer(server_address, MyHandler)
     print(f"The server is running: http://localhost:{PORT}")
     httpd.serve_forever()
