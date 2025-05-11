96eef2c6051abcb3e6cb6c6fafa61db2fa51bf9e
wizardrad@gmail.com <wizardrad@gmail.com>
2025-05-11 17:32:50 +0500

    about, chat, form added

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    name = None
+    if request.method == "POST":
+        name = request.form.get("name")
+    return render_template("form.html", name=name)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
